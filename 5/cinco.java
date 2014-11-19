d7decd07ea3d00d2bfdf5481d04f6e6f7d83402e
Ivan Murillo <murilloivan@gmail.com>
2014-11-19 21:56:28 +0100

    fifth prob

---
+++ 5/cinco.java
@@ -0,0 +1,13 @@
+public class cinco {
+    public static void main(String[] args) {
+
+    		/*
+    		*Create a program that counts how many seconds there are in a year. 
+    		*You can assume that a year consists of 365 days (therefore the year is not a leap year).
+    		*/
+    		
+    		int segAño = 60*60*24*365;
+
+        System.out.println("Hay " + segAño + " segundos en un año");
+      }
+}
