6e302a7f2ce5f776236caf57c2bbe7ff9696798e
Ivan Murillo <murilloivan@gmail.com>
2014-12-24 12:29:19 +0100

    27

---
+++ 27/Veintisiete.java
@@ -0,0 +1,15 @@
+/*27*/
+
+class Veintisiete
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Create a program that prints the integers (whole numbers) from 1 to 100.");
+		int a = 1;
+		
+		while(a<101){
+			System.out.println(a);
+			a++;
+		}
+	}
+}
