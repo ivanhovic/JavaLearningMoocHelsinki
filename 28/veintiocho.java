0eec4c9ae91f4bb8a43bb57efb54fae381edb720
Ivan Murillo <murilloivan@gmail.com>
2014-12-24 12:25:26 +0100

    28

---
+++ 28/veintiocho.java
@@ -0,0 +1,15 @@
+/*28*/
+
+class veintiocho
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Create a program that prints the integers (whole numbers) from 100 to 1.");
+		int a = 100;
+		
+		while(a>0){
+			System.out.println(a);
+			a--;
+		}
+	}
+}
